437db0ae641a5384ef84ab8faa625544c87c52ae
schonssj <jvschons@gmail.com>
2025-04-27 22:02:58 -0300

    chore: add mock data for users table

---
+++ 2-sql/2-2/data.sql
@@ -0,0 +1,5 @@
+INSERT INTO users (id, email, name) VALUES
+(1, 'john@example.com', 'John Doe'),
+(2, 'jane@example.com', 'Jane Smith'),
+(3, 'john@example.com', 'John Duplicate'),
+(4, 'alice@example.com', 'Alice');
